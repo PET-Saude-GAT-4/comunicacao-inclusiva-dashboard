0a96f79e56c3033aca64916b1ad567f1e1224000
NicoStone-Dev <nicostone.rocha@gmail.com>
2026-09-14 14:07:16 -0300

    styles: resize pictogram cards

--- src/app/(authenticated)/components/PictogramCard/PictogramCard.tsx
+++ src/app/(authenticated)/components/PictogramCard/PictogramCard.tsx
@@ -9,32 +9,32 @@ type Props = {
 
 export default function PictogramCard({ pictogram }: Props) {
   return (
-    <div className="flex flex-col gap-3 p-5 bg-white rounded-lg border border-outline-common shadow-sm text-text-on-primary min-w-0">
-      <div className="flex w-full justify-end ">
+    <div className="flex flex-col gap-2 p-3 bg-white rounded-md border border-outline-common shadow-sm text-text-on-primary min-w-0">
+      <div className="flex w-full justify-end">
         <button
           type="button"
           className="text-gray-600 hover:text-gray-900 shrink-0"
         >
-          <MdMoreVert size={24} />
+          <MdMoreVert size={20} />
         </button>
       </div>
-      <div className="flex items-start justify-between gap-5">
-        <div className="flex flex-col items-center justify-center w-full gap-5 min-w-0">
-          <div className="flex items-center justify-center w-32 h-32 shrink-0 rounded-full bg-surface-secondary-dark text-white overflow-hidden">
+      <div className="flex items-start justify-between gap-3">
+        <div className="flex flex-col items-center justify-center w-full gap-3 min-w-0">
+          <div className="flex items-center justify-center w-24 h-24 shrink-0 rounded-full bg-surface-secondary-dark text-white overflow-hidden">
             {pictogram.fileUrl ? (
               <Image
                 src={pictogram.fileUrl}
                 alt={pictogram.description}
-                width={128}
-                height={128}
+                width={96}
+                height={96}
                 className="w-full h-full object-cover"
               />
             ) : (
-              <MdImage size={64} />
+              <MdImage size={48} />
             )}
           </div>
-          <div className="flex flex-col gap-2 min-w-0 w-full">
-            <h3 className="text-heading font-semibold truncate text-center">
+          <div className="flex flex-col gap-1 min-w-0 w-full">
+            <h3 className="text-body-emph font-semibold truncate text-center">
               {pictogram.description ?? "Pictogram"}
             </h3>
             <div className="flex justify-center">
@@ -43,8 +43,8 @@ export default function PictogramCard({ pictogram }: Props) {
           </div>
         </div>
       </div>
-      <hr className="border-t border-outline-common my-1" />
-      <div className="flex flex-col gap-1 items-center">
+      <hr className="border-t border-outline-common" />
+      <div className="flex flex-col gap-xs items-center text-body">
         <span className="font-bold">Data de Criação</span>
         <span>{new Date(pictogram.createdAt).toLocaleDateString("pt-BR")}</span>
       </div>
